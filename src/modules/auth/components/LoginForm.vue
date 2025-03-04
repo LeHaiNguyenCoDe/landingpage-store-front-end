fc1a677ab1ef4db87978b0b4a883de03133500b0
hai.nguyen <hai.nguyen@codihaus.com>
2025-03-04 13:20:15 +0700

    fix: hostFix

--- src/modules/auth/components/LoginForm.vue
+++ src/modules/auth/components/LoginForm.vue
@@ -2,7 +2,7 @@
   <form @submit.prevent="submitForm">
     <div>
       <label>Email:</label>
-      <input type="user" v-model="user" required />
+      <input type="user" v-model="username" required />
     </div>
     <div>
       <label>Password:</label>
@@ -16,18 +16,18 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
-const user = ref('');
+const username = ref('');
 const password = ref('');
 const router = useRouter();
 
 const submitForm = () => {
-  const userData = { email: user.value, password: password.value, token: 'fake-token' };
+  const userData = { username: username.value, password: password.value, token: 'fake-token' };
   emit('login', userData);
   router.push('/dashboard');
 };
 import { defineEmits } from 'vue';
 
 const emit = defineEmits<{
-  (e: 'login', userData: { email: string; password: string; token: string }): void;
+  (e: 'login', userData: { username: string; password: string; token: string }): void;
 }>();
 </script>
