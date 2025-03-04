ea5cc7a62021fc9af58000b4652b44f3dcaad3a0
hai.nguyen <hai.nguyen@codihaus.com>
2025-03-04 14:30:30 +0700

    feat: ceate route

--- src/modules/home_page/router/index.ts
+++ src/modules/home_page/router/index.ts
@@ -1,11 +1,20 @@
 import type { RouteRecordRaw } from 'vue-router';
+import MainLayout from '@/layouts/MainLayout.vue';
+import HomePage from '@/modules/home_page/views/HomePage.vue';
 
 const authRoutes: RouteRecordRaw[] = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/modules/home_page/views/HomePage.vue'),
-  }
+    path: '/Home',
+    component: MainLayout,
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: HomePage,
+        meta: { module: 'home' },
+      },
+    ],
+  },
 ];
 
 export default authRoutes;
--- src/modules/sample/router/index.ts
+++ src/modules/sample/router/index.ts
@@ -1,10 +1,19 @@
 import type { RouteRecordRaw } from 'vue-router';
+import MainLayout from '@/layouts/MainLayout.vue';
+import HomePage from '@/modules/home_page/views/HomePage.vue';
 
 const authRoutes: RouteRecordRaw[] = [
   {
     path: '/sample',
-    name: 'sample',
-    component: () => import('../views/SamplePage.vue'),
+    component: MainLayout,
+    children: [
+      {
+        path: '/',
+        name: 'sample',
+        component: HomePage,
+        meta: { module: 'sample' },
+      },
+    ],
   },
 ];
 
